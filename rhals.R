06ad3a7108e8e20a3c30e4f16efb6f8c05d56814
parkhwansoo <02ghkstn@naver.com>
2024-09-09 16:19:17 +0900

    Add files via upload

---
+++ rhals.R
@@ -0,0 +1,43 @@
+
+library(installr)
+
+check.for.updates.R()
+#install.R()
+#주석처리 설명 worlds
+#a<-1 a=1 1-> a 같음 상관없음
+a=1
+a1=10
+b=3
+a+b
+print(a+b)
+#디버깅=흔적들을 찾아서 오류를 찾는 것
+print("test")
+print("10")
+print(99)
+print(a)
+2^3 #2의 3제곱
+97%%4 #나머지 값
+4522%/%75 #나누기 결과 정수
+a<=b
+#a=b / a==b / a!=b * 데이터 분석 핵심
+a!=b
+#둘중 하나 or \| , 0 and 1 일때 둘다 만족하면 
+#istrue 
+a<= 60|b==30
+a<=45&b>=2
+isTRUE(a1>10)#tab 자동완성
+
+1+2+3+4+5+6+7+8+9+10
+(1+2+3+4+5+6+7+8+9+10)/10
+(1+2+3+4+5)/5
+#결측값
+integer_var <- 5
+numeric_var <- 3.14
+complex_var <- 2+3i
+character_var <-"hello world"
+logical_var <- TRUE
+NULL_VAR <- NA
+negative_inf<- -inf
+NA_var<- NA
+positive_inf<- Inf
+nan <- 0/0
